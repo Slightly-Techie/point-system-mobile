f5dc4283a3d1aff2b04e97efa061f8c91a566d3e
iamsirmike <asamoahmichael77@gmail.com>
2023-07-20 16:23:43 +0000

    add sample widget

---
+++ lib/src/views/widgets/sample_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class SampleWidegt extends StatelessWidget {
+  const SampleWidegt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
